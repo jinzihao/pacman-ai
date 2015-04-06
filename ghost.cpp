42a51476a7705787086cdefb0c83fcfcc3cd5c59
jhtdavid <jhtdavid960524@sina.com>
2015-04-06 16:30:58 +0800

    ghost

---
+++ ghost.cpp
@@ -0,0 +1,17 @@
+
+//bean get_ghost_last_action();
+
+vertex* get_ghost_location();
+
+void dummy_ghost_action() //dummy ai-cation
+{
+	
+	if (location_type=vertex)
+	{
+		
+	}
+	else (location_type=edge)
+	
+	
+  
+} 
